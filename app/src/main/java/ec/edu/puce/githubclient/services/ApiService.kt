4489a06148c794e7fd8572dca15f8472044acf0e
Usuario invitado <Guest@iMac-A10819.PUCE.EDU.EC>
2026-05-21 14:38:00 -0500

    Laboratorio 2

--- app/src/main/java/ec/edu/puce/githubclient/services/ApiService.kt
+++ app/src/main/java/ec/edu/puce/githubclient/services/ApiService.kt
@@ -2,8 +2,15 @@ package ec.edu.puce.githubclient.services
 
 import ec.edu.puce.githubclient.models.Repository
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET( value = "/user/repos")
-    suspend fun getRepository () : List <Repository>
+    suspend fun getRepository (
+        @Query( value = "sort") created : String = "created",
+        @Query( value = "direction") direction : String = "desc",
+        @Query( value = "affiliation") affiliation : String = "owner",
+        @Query( value = "per_page") perPage : Int = 100,
+        @Query( value = "t") t: String = "${System.currentTimeMillis()}",
+    ) : List<Repository>
 }
